79efe38f8759b76c6a49302d49147f0fc330c074
Brian Antunes <brian.antunes@yahoo.com>
2022-07-10 21:19:27 -0400

    Updated main.go, added Static page

--- main.go
+++ main.go
@@ -3,27 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
-    "os"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fs)
+	http.HandleFunc("/call", handler)
+
 	fmt.Println("listening...")
- 	err := http.ListenAndServe(GetPort(), nil)
- 	if err != nil {
- 		log.Fatal("ListenAndServe: ", err)
- 	}
+	err := http.ListenAndServe(GetPort(), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 const (
-    content = `<?xml version="1.0" encoding="UTF-8"?>
+	content = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
     <Gather>
         <Say>Hello Monkey, please press 1</Say>
     </Gather>
 </Response>`
-    echoMsg = `<?xml version="1.0" encoding="UTF-8"?>
+	echoMsg = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
 	<Say>Hello, World!</Say>
     <Play>https://api.twilio.com/Cowbell.mp3</Play>
@@ -32,23 +36,23 @@ const (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/xml")
-    
-    rec := r.FormValue("Digits")
 
-    if rec == "" {
+	rec := r.FormValue("Digits")
+
+	if rec == "" {
 		fmt.Fprint(w, content)
 		return
 	}
 
-    fmt.Fprint(w, echoMsg)
+	fmt.Fprint(w, echoMsg)
 }
 
- func GetPort() string {
- 	var port = os.Getenv("PORT")
- 	// Set a default port if there is nothing in the environment
- 	if port == "" {
- 		port = "4747"
- 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
- 	}
- 	return ":" + port
- }
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
